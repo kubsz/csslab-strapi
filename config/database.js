4c3868b70320d959f2452d4ce357d27fe463dd26
Kubs Korkmaz <me@kubs.uk>
2022-04-30 13:09:16 +0100

    updated database env vars

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "csslab-strapi"),
-      user: env("DATABASE_USERNAME"),
+      database: env("DATABASE_NAME", "csslab"),
+      user: env("DATABASE_USER"),
       password: env("DATABASE_PASSWORD"),
       ssl: env.bool("DATABASE_SSL", false),
     },
