964456a5f243ed8e59c39a72269069f19dd6027c
Kubs <me@kubs.uk>
2022-07-02 22:33:42 +0100

    added variables to component schema

--- src/api/component/controllers/component.js
+++ src/api/component/controllers/component.js
@@ -12,7 +12,7 @@ module.exports = createCoreController("api::component.component", ({ strapi }) =
   async find(ctx) {
     const query = {
       ...ctx.query,
-      populate: { props: true, tags: true, technologies: { populate: { logo: true } }, category: true },
+      populate: { props: true, variables: true, tags: true, technologies: { populate: { logo: true } }, category: true },
     };
     return await strapi.entityService.findMany(uid, query);
   },
